38580d0b83f8b80028b8accdac6adfa9a8d63b8a
SYTHA-PC\VP.Start <phearun@vpstart.com>
2023-05-20 12:04:07 +0700

    dropdown navbar

--- script.js
+++ script.js
@@ -4,45 +4,47 @@ showSlide();
 
 var timer = setInterval(showSlide, 3000);
 
-var slideContainer = document.getElementById('slideshow-container');
-var slideControl = document.getElementById('slide-control');
+var slideContainer = document.getElementById("slideshow-container");
+var slideControl = document.getElementById("slide-control");
+let cls_Btn = document.querySelector(".cls_btn");
+slideContainer.onmouseover = function () {
+  clearInterval(timer);
+};
 
-slideContainer.onmouseover = function() {
-	clearInterval(timer);
-}
-
-slideContainer.onmouseleave = function() {
-	timer = setInterval(showSlide, 3000);
-}
-
+slideContainer.onmouseleave = function () {
+  timer = setInterval(showSlide, 3000);
+};
 
 function showSlide() {
-	var i;
-	var slides = document.getElementsByClassName('slide-item');
-	for (var i = 0; i < slides.length; i++) {
-		slides[i].style.display = 'none';
-	}
-	
-	slideIndex++;
-	if (slideIndex > slides.length) slideIndex = 1;
-	slides[slideIndex-1].style.display = "block";
+  var i;
+  var slides = document.getElementsByClassName("slide-item");
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) slideIndex = 1;
+  slides[slideIndex - 1].style.display = "block";
 }
 
 function chooseSlide(n) {
-	slideIndex = n;
-	showSlide();
-	clearInterval(timer);
+  slideIndex = n;
+  showSlide();
+  clearInterval(timer);
 }
 
 function showMenu() {
-	var topNav = document.getElementById('topnav');
-	if (topNav.className === "navbar") {
-		topNav.className += " show";
-	} else {
-		topNav.className = "navbar";
-	}
+  var topNav = document.getElementById("topnav");
+  if (topNav.className === "navbar") {
+    topNav.className += " show";
+  } else {
+    topNav.className = "navbar";
+  }
 }
 // open dropdown our product
 function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-  }
+  document.getElementById("myNav").style.height = "100%";
+}
+function closeNav() {
+  document.getElementById("myNav").style.height = "0";
+}
